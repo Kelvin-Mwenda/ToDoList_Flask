e11e0529b1ee4903cb6058a8d258c9987af809ea
Kelvin <kelvinmwendadoreen@gmail.com>
2024-08-10 09:38:51 +0300

    update..

--- website/models.py
+++ website/models.py
@@ -15,6 +15,6 @@ class User(db.Model,UserMixin):
 class Event(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     event=db.Column(db.String(1000))
-    date = db.Column(db.Date)
+    date = db.Column(db.Date,default=datetime.utcnow)
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
     
